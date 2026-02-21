a69785786aa7ca6c0e69fd43ab18c63d0039fe18
cjy0812 <cjy081219@Gmail.com>
2026-02-22 01:05:27 +0800

    feat: 脚本_用户可自定义审查工具域名

--- url-tools-script/url-redirect.user.js
+++ url-tools-script/url-redirect.user.js
@@ -1,16 +1,32 @@
 // ==UserScript==
-// @name         GKD Redirect by ChatGPT
-// @namespace    your.namespace
-// @version      1.1
-// @description  Redirect i.gkd.li and patch device redirect
+// @name         GKD快照审查器自定义域
+// @namespace    ling
+// @version      1.2
+// @description  重定向 i.gkd.li 并使用自定义域配置修补设备重定向
 // @match        https://i.gkd.li/*
 // @run-at       document-start
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_registerMenuCommand
 // ==/UserScript==
 
 (function () {
   'use strict';
 
-  const customDomain = 'https://li.chenge.eu.org'; // 修改成你自己的审查工具域名
+  // 注册菜单命令，让用户配置域名
+  GM_registerMenuCommand('设置自定义域名', function () {
+    const customDomain = prompt(
+      '请输入自定义域名:',
+      GM_getValue('customDomain', 'https://li.chenge.eu.org'),
+    );
+    if (customDomain && customDomain !== GM_getValue('customDomain')) {
+      GM_setValue('customDomain', customDomain); // 保存用户配置
+      alert('自定义域名已更新为: ' + customDomain);
+    }
+  });
+
+  // 获取自定义域名配置，默认值为 'https://li.chenge.eu.org'
+  const customDomain = GM_getValue('customDomain', 'https://li.chenge.eu.org');
 
   const url = new URL(location.href);
 
@@ -19,7 +35,6 @@
   // ===============================
   if (url.pathname.startsWith('/i/')) {
     const id = url.pathname.replace('/i/', '');
-
     if (id) {
       const target = `${customDomain}/${id}`;
       location.replace(target);
@@ -32,7 +47,7 @@
   // ===============================
   if (url.pathname === '/device') {
     if (url.hostname === 'i.gkd.li') {
-      url.hostname = 'li.chenge.eu.org';
+      url.hostname = new URL(customDomain).hostname; // 使用自定义域名的 host
       location.replace(url.href);
       return;
     }
